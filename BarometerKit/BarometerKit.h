d5b5a66daf7db969c756d1d296b6ed699950dcde
Bruno Godbout <bruno.godbout@hapticsoft.com>
2018-08-01 14:33:45 -0400

    Initial Commit

---
+++ BarometerKit/BarometerKit.h
@@ -0,0 +1,19 @@
+//
+//  BarometerKit.h
+//  BarometerKit
+//
+//  Created by Bruno Godbout on 2018-08-01.
+//  Copyright © 2018 Haptic Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BarometerKit.
+FOUNDATION_EXPORT double BarometerKitVersionNumber;
+
+//! Project version string for BarometerKit.
+FOUNDATION_EXPORT const unsigned char BarometerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BarometerKit/PublicHeader.h>
+
+
