b0bc04fab7ebe3dfd3670302cc0c03724ea97cb4
Bruno Godbout <bruno.godbout@hapticsoft.com>
2018-08-02 16:12:13 -0400

    Initial Commit

---
+++ BarometerKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarometerKitDemo
+//
+//  Created by Bruno Godbout on 2018-08-02.
+//  Copyright © 2018 Haptic Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
