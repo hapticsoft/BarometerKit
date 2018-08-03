a56f6ef52010311c4a8ef3ee1f986fae12395f20
Bruno Godbout <bruno.godbout@hapticsoft.com>
2018-08-03 09:40:08 -0400

    Added outlets for kPaValueLabel and inHgValueLabel.

--- BarometerKitDemo/ViewController.swift
+++ BarometerKitDemo/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var kPaValueLabel: UILabel!
+    @IBOutlet weak var inHgValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
