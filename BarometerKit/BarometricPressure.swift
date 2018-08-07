936dc8f928fb0a162e876fc9cd3faf628751a63e
Bruno Godbout <bruno.godbout@hapticsoft.com>
2018-08-07 14:24:06 -0400

    Added support for converting kPa to inHg, hPa, mBar, mmHg, torr, atm and psi.

--- BarometerKit/BarometricPressure.swift
+++ BarometerKit/BarometricPressure.swift
@@ -8,14 +8,51 @@
 
 import Foundation
 
+/// Encapsulates barometric pressure values and provide conversion between units of measurements.
+/// - SeeAlso: [weather.gov](https://www.weather.gov/media/epz/wxcalc/pressureConversion.pdf "Pressure conversion formulas")
+/// - SeeAlso: [unit juggler](https://www.unitjuggler.com/pressure-conversion.html "Pressure conversion")
 public struct BarometricPressure {
+    
+    /// Pressure measured in kilopascals
     public let kPa: NSNumber
     
+    /// Initializes and returns a `BarometricPressure` value with the specified kilopascal value.
     public init(kPa: NSNumber) {
         self.kPa = kPa
     }
     
+    /// Pressure measured in inches of mercury
     public var inHg: NSNumber {
-        return NSNumber(value: kPa.floatValue * 0.295300)
+        return NSNumber(value: kPa.doubleValue * 0.295301)
+    }
+    
+    /// Pressure measured in hectopascals
+    public var hPa: NSNumber {
+        return NSNumber(value: kPa.doubleValue * 10)
+    }
+    
+    /// Pressure measured in millibars
+    public var mBar: NSNumber {
+        return hPa
+    }
+    
+    /// Pressure measured in millimeters of mercury
+    public var mmHg: NSNumber {
+        return NSNumber(value: kPa.doubleValue * 7.5006375541921)
+    }
+    
+    /// Pressure measured in torrs
+    public var torr: NSNumber {
+        return mmHg
+    }
+    
+    /// Pressure measured in standard atmospheres
+    public var atm: NSNumber {
+        return NSNumber(value: kPa.doubleValue * 0.0098692326671601)
+    }
+    
+    /// Pressure measured in pounds per square inch
+    public var psi: NSNumber {
+        return NSNumber(value: kPa.doubleValue * 0.14503773773022)
     }
 }
--- BarometerKitTests/BarometerKitTests.swift
+++ BarometerKitTests/BarometerKitTests.swift
@@ -91,5 +91,44 @@ class BarometerKitTests: XCTestCase {
         
     }
 
+    func test_kPa() {
+        let kPa = NSNumber(value: 100)
+        let pressure = BarometricPressure(kPa: kPa)
+        XCTAssertEqual(pressure.kPa, kPa)
+    }
     
+    func test_inHg() {
+        let pressure = BarometricPressure(kPa: NSNumber(value: 100))
+        XCTAssertEqual(pressure.inHg.stringValue, NSNumber(value: 29.5301).stringValue)
+    }
+    
+    func test_hPa() {
+        let pressure = BarometricPressure(kPa: NSNumber(value: 100))
+        XCTAssertEqual(pressure.hPa.stringValue, NSNumber(value: 1000).stringValue)
+    }
+    
+    func test_mBar() {
+        let pressure = BarometricPressure(kPa: NSNumber(value: 100))
+        XCTAssertEqual(pressure.mBar.stringValue, NSNumber(value: 1000).stringValue)
+    }
+    
+    func test_mmHg() {
+        let pressure = BarometricPressure(kPa: NSNumber(value: 100))
+        XCTAssertEqual(pressure.mmHg.stringValue, NSNumber(value: 750.06375541921).stringValue)
+    }
+    
+    func test_torr() {
+        let pressure = BarometricPressure(kPa: NSNumber(value: 100))
+        XCTAssertEqual(pressure.torr.stringValue, NSNumber(value: 750.06375541921).stringValue)
+    }
+    
+    func test_atm() {
+        let pressure = BarometricPressure(kPa: NSNumber(value: 100))
+        XCTAssertEqual(pressure.atm.stringValue, NSNumber(value: 0.98692326671601).stringValue)
+    }
+    
+    func test_psi() {
+        let pressure = BarometricPressure(kPa: NSNumber(value: 100))
+        XCTAssertEqual(pressure.psi.stringValue, NSNumber(value: 14.503773773022).stringValue)
+    }
 }
